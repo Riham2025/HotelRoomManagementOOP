c9b9c7f95b0632e8233506fb8e009aa9a8274ee2
riham <riham91.alsiyabi@gmail.com>
2025-07-17 11:59:25 +0400

    craete new class call Room

---
+++ Room.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelRoomManagement
+{
+    // Room class to store room information
+    class Room
+    {
+        //basic properties
+        
+        public int RoomNumber { get; set; } 
+        public double DailyRate { get; set; }
+        public bool IsReserved { get; set; }
+    }
+}
