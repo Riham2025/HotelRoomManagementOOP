b8a9292d14378c7474ad0a467f16dee3370c2bba
riham <riham91.alsiyabi@gmail.com>
2025-07-17 15:01:32 +0400

    Display user menu

--- Program.cs
+++ Program.cs
@@ -7,5 +7,19 @@
             // Create a HotelManager object to manage the hotel system
             HotelManager manager = new HotelManager();
 
+            bool running = true;
+            while (running)
+            {
+                // Display user menu
+                Console.WriteLine("\n==== Hotel Room Management ====");
+                Console.WriteLine("1. Add New Room");
+                Console.WriteLine("2. View All Rooms");
+                Console.WriteLine("0. Exit");
+                Console.Write("Select an option: ");
+
+                string choice = Console.ReadLine();
+
+
+            }
         }
     }
