af31fc296150af2d0d2561918714faed03fbef11
riham <riham@DESKTOP-BDRD4NF>
2025-07-17 20:57:11 +0400

    Reserve a room

--- Room.cs
+++ Room.cs
@@ -17,6 +17,13 @@ namespace HotelRoomManagement
         public string GuestName { get; set; } // Stores the guest's name
         public int Nights { get; set; }       // Stores number of nights reserved
 
+        public void Reserve(string guestName, int nights) //Reserve a room
+        {
+            GuestName = guestName;
+            Nights = nights;
+            IsReserved = true;
+        }
+
         // Constructor to initialize a new room
         public Room(int roomNumber, double dailyRate)
         {
