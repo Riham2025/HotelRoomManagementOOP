d727bdcce9b8345dde579202d396eea9da44a92c
riham <riham91.alsiyabi@gmail.com>
2025-07-17 15:03:28 +0400

    Handle user input

--- Program.cs
+++ Program.cs
@@ -19,7 +19,37 @@
 
                 string choice = Console.ReadLine();
 
+                // Handle user input
+                switch (choice)
+                {
+                    case "1":
+                        Console.Write("Enter Room Number: ");
+                        int roomNumber = int.Parse(Console.ReadLine()); // Get room number
+
+                        Console.Write("Enter Daily Rate: ");
+                        double dailyRate = double.Parse(Console.ReadLine()); // Get rate
+
+                        manager.AddRoom(roomNumber, dailyRate); // Call AddRoom
+                        break;
+
+                    case "2":
+                        manager.ViewRooms(); // Show all rooms
+                        break;
+
+                    case "0":
+                        running = false; // Exit the loop
+                        Console.WriteLine("Exiting...");
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid option."); // Handle unknown input
+                        break;
+
+                }
 
             }
+
+
         }
+        
     }
