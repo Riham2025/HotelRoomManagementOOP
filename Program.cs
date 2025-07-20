db152ceadcdddac3c94370afa35d24c22911124e
riham <riham91.alsiyabi@gmail.com>
2025-07-20 09:33:59 +0400

    Add a new option to the menu:

--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@
                 Console.WriteLine("\n==== Hotel Room Management ====");
                 Console.WriteLine("1. Add New Room");
                 Console.WriteLine("2. View All Rooms");
+                Console.WriteLine("3. Reserve a Room");
                 Console.WriteLine("0. Exit");
                 Console.Write("Select an option: ");
 
