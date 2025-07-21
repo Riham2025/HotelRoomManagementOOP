29af265c73c20a707e746cd0ad792b5e62923cf4
riham <riham91.alsiyabi@gmail.com>
2025-07-21 14:10:16 +0400

    Create a new class called Reservation

---
+++ Reservation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelRoomManagement
+{
+    class Reservation
+    {
+        
+        public string GuestName { get; set; } 
+        public int Nights { get; set; } 
+        public DateTime ReservationDate { get; set; } 
+    }
+}
