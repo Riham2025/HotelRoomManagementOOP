520258f64cb92d36f765d2863218b789c6a20150
riham <riham91.alsiyabi@gmail.com>
2025-07-21 15:19:04 +0400

    last update

--- Room.cs
+++ Room.cs
@@ -6,20 +6,24 @@ using System.Threading.Tasks;
 
 namespace HotelRoomManagement
 {
+   
+
     // Room class to store room information
     class Room
     {
+        public List<Reservation> Reservations { get; set; } = new List<Reservation>(); //// List to store reservations for the room
+
 
         public int RoomNumber { get; set; } // Property: Room number (unique identifier)
         public double DailyRate { get; set; } // Property: Daily rate for the room
-        //public bool IsReserved { get; set; } // Property: Reservation status (true if reserved)
+        public bool IsReserved { get; set; } // Property: Reservation status (true if reserved)
 
-        //public string GuestName { get; set; }  // Stores the guest's name
-       // public int Nights { get; set; }      // Stores number of nights reserved
+        public string GuestName { get; set; }  // Stores the guest's name
+        public int Nights { get; set; }      // Stores number of nights reserved
 
-       // public DateTime ReservationDate { get; set; } // Stores the date of reservation
+        public DateTime ReservationDate { get; set; } // Stores the date of reservation
 
-        public List<Reservation> Reservations { get; set; } = new List<Reservation>(); // List to store reservations for the room
+         
 
 
         public void Reserve(string guestName, int nights, DateTime reservationDate)
@@ -57,6 +61,7 @@ namespace HotelRoomManagement
 
 
 
+
         // Method to calculate total cost of reservation
         public double GetTotalCost()
 
@@ -85,6 +90,8 @@ namespace HotelRoomManagement
             Reservations.Add(new Reservation(guestName, nights, date));
             return true;
         }
+
+        
 
 
 
